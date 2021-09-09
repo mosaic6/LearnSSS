90e54eccc6850d1ae5b850f0aec84deff0b82985
Guanglei Liu <guanglei1025@gmail.com>
2021-09-08 22:05:28 -0400

    added product model

--- Sources/Models/Product.swift
+++ Sources/Models/Product.swift
@@ -7,25 +7,11 @@
 
 import Foundation
 
-final class Products: Model {
-    // Name of the table or collection
-    static let schema = "products"
-
-    // Unique identifier for this Product.
-    @ID(key: .id)
-    var id: UUID?
-
-    // The Galaxy's name.
-    @Field(key: "name")
+struct Product: Content {
+    var id: Int
     var name: String
-
-    // Creates a new, empty Product.
-    init() { }
-
-    // Creates a new Product with all properties set.
-    init(id: UUID? = nil, name: String) {
-        self.id = id
-        self.name = name
-    }
-    
+    var description: String
+    var imageURL: String
+    var price: String
+    var quantity: Int
 }
