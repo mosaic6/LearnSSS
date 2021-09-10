dac27c8bcec3e59c418c1b05c0b12afbc0f7278a
Guanglei Liu <guanglei1025@gmail.com>
2021-09-09 22:01:08 -0400

    created submit shopping cart API

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -26,4 +26,14 @@ func routes(_ app: Application) throws {
         return product
     }
 
+    // MARK: - Shopping Cart
+    let shoppingCart = app.grouped("shoppingCart")
+
+    /// Submit orders in the shopping cart
+    shoppingCart.post("submit") { req -> HTTPStatus in
+        let shoppingCart = try req.content.decode(ShoppingCart.self)
+        print(shoppingCart.orders.count)
+        // FIXME: Check with data base and ensure there is enough project.
+        return .ok
+    }
 }
